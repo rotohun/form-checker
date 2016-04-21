a357af04943084cee4c4643260708d795a53f238
Ronald Hunter <ronald@ronaldhunter.co>
2016-04-21 12:29:22 -0400

    Added steps to take for the project

---
+++ main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+	
+	// Hide hints.
+
+	//When event happenes on password input .
+		//Find out if password is valid.
+			//Hide hint if match.
+			//Else show hint.
+
+	//When event happens on confirmation input.
+		//Find out if confirmation and input match.
+			//Hide hint if match.
+			//Else show hint.
+
+
+});
