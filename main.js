cc5fb521d2657de89d8fc126bf3abf8128b1b087
Ronald Hunter <ronald@ronaldhunter.co>
2016-04-21 13:14:14 -0400

    added password checker that show the hints and hides them depending on password lenght and copy

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 jQuery(document).ready(function($) {
 	
+var $password = $('#password');
+var $confirmPass = $('#confirm_password');
 	// Hide hints.
+	$('form span').hide();
 
-	//When event happenes on password input .
+
+	//When event happenes on password input.
+	function passwordEvent() {
 		//Find out if password is valid.
+		if ($password.val().length > 8) {
 			//Hide hint if match.
-			//Else show hint.
 
-	//When event happens on confirmation input.
-		//Find out if confirmation and input match.
-			//Hide hint if match.
+			$password.next().hide();
+
+		}else{
 			//Else show hint.
+			$password.next().show();
+		}
+	}
+
+
+	function confirmPassword(argument) {
+		//Find out if confirmation and input match.
+		if ($password.val() === $confirmPass.val()) {
+			//Hide hint if match.
+			$confirmPass.next().hide();
+
+		}else{
+			//Else show hint.
+			$confirmPass.next().show();
+		}
+	}
+
+
+	$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPassword).keydown(confirmPassword);
+	//When event happens on confirmation input.
+	$confirmPass.focus(confirmPassword).keyup(confirmPassword);
+
 
 
 });
